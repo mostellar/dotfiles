05305045d6672dab187c1c03b404207461386ccf
Will Mostellar <stellarwill@gmail.com>
2019-12-09 21:02:22 -0500

    adding oh-my-zsh custom files

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,3 @@
+alias rm='rm -i'
+alias mv='mv -i'
+alias dotfiles='$(which git) --git-dir=$HOME/.config/dotfiles.git/ --work-tree=$HOME'
---
+++ .oh-my-zsh/custom/path.zsh
@@ -0,0 +1,1 @@
+export PATH="/Users/willmostellar/projects/scripts:/usr/local/opt/python/libexec/bin:$PATH"
