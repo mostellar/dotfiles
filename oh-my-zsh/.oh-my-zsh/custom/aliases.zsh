fbb37a1df72d8a9157e9c510b4007d0dbd2d4be6
Will Mostellar <stellarwill@users.noreply.github.com>
2020-12-11 13:00:27 -0500

    Change dotfiles alias for new stow directory

--- oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,7 +1,8 @@
 alias rm='rm -i'
 alias mv='mv -i'
 alias cp='cp -i'
-alias dotfiles='$(which git) --git-dir=$HOME/.config/dotfiles.git/ --work-tree=$HOME'
+# alias dotfiles='$(which git) --git-dir=$HOME/.config/dotfiles.git/ --work-tree=$HOME'
+alias dotfiles='$(which git) -C "$HOME/.dotfiles/"'
 alias bloggle="blueutil -p 0; printf 'Resetting Bluetooth'; sleep 1; printf '.'; sleep 1; printf '.'; sleep 1; printf '.'; sleep 1; printf '.'; sleep 1; printf '.\n'; blueutil -p 1"
 alias clear="clear && printf '\e[3J'"
 alias focus="killall OneDrive; killall CrashPlanService; killall CrashPlan; killall callservicesd; killall hubd; killall soagent"
