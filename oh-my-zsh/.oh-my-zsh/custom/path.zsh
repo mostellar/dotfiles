f83f15b27c278eb591a0458b3686febc73ae9361
Will Mostellar <26006367+mostellar@users.noreply.github.com>
2021-12-16 20:07:36 -0500

    Add mysql to path

--- oh-my-zsh/.oh-my-zsh/custom/path.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$HOME/.local/bin:$HOME/scripts:/usr/local/opt/python/libexec/bin:$PATH"
+export PATH="/usr/local/mysql/bin:$HOME/.local/bin:$HOME/scripts:/usr/local/opt/python/libexec/bin:$PATH"
