c4bb2cff3500b0ba685f8c8194c0da5ca2464f5c
Will Mostellar <26006367+mostellar@users.noreply.github.com>
2022-08-02 14:10:11 -0400

    Fix path

--- oh-my-zsh/.oh-my-zsh/custom/path.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/mysql/bin:$HOME/.local/bin:$HOME/scripts:/usr/local/opt/python/libexec/bin:$PATH"
-echo 'export PATH="/opt/homebrew/bin:$PATH"' >> ~/.zshrc
+export PATH="/opt/homebrew/bin:/usr/local/mysql/bin:$HOME/.local/bin:$HOME/scripts:/usr/local/opt/python/libexec/bin:$PATH"
+#echo 'export PATH="/opt/homebrew/bin:$PATH"' >> ~/.zshrc
